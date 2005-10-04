254da34016148a8f75081f3dac9775020f3940ff
Olivier Thauvin <nanardon@zarb.org>
2005-10-04 04:15:52 +0000

    - 0.01
    git-svn-id: svn+ssh://haiku.zarb.org/home/projects/rpm4/svn/trunk@37 971eb68f-4bfb-0310-8326-d2484c010a4c

--- lib/RPM4.pm
+++ lib/RPM4.pm
@@ -30,7 +30,7 @@ use RPM4::Header::Dependencies;
 use RPM4::Header::Files;
 use RPM4::Spec;
 
-our $VERSION = '0.08';
+our $VERSION = '0.01';
 our @ISA = qw(DynaLoader Exporter);
 our @EXPORT = qw(moduleinfo
                  readconfig querytag tagName tagValue expand rpmvercmp
