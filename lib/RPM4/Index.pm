a1dff1764158b369b90d58c46f23eee8fa55d627
Olivier Thauvin <nanardon@zarb.org>
2005-11-16 20:59:53 +0000

    - use MDV:: module
    git-svn-id: svn+ssh://haiku.zarb.org/home/projects/rpm4/svn/trunk@48 971eb68f-4bfb-0310-8326-d2484c010a4c

--- lib/RPM4/Index.pm
+++ lib/RPM4/Index.pm
@@ -23,7 +23,7 @@ use warnings;
 
 use RPM4;
 use RPM4::Header;
-use Packdrakeng;
+use MDV::Packdrakeng;
 
 use File::Temp qw(tempfile);
 
@@ -35,7 +35,7 @@ sub buildindex {
         $pid = open($h_synthesis, "| gzip --best > '$options{synthesis}'") or return 0;
     }
     if ($options{hdlist}) {
-        $pack = Packdrakeng->new(
+        $pack = MDV::Packdrakeng->new(
             archive => $options{hdlist},
             comp_level => $options{complevel},
         ) or return 0;
@@ -104,7 +104,7 @@ sub buildhdlist {
 
 sub parsehdlist {
     my (%options) = @_;
-    my $pack = Packdrakeng->open(archive => $options{hdlist}) or return 0;
+    my $pack = MDV::Packdrakeng->open(archive => $options{hdlist}) or return 0;
 
     my (undef, $files, undef) = $pack->getcontent();
     pipe(my $in, my $out);
