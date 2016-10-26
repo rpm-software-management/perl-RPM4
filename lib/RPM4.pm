cf8c762cf1778d6abe2596c1a14774761db479b9
Thierry Vignaud <thierry.vignaud@gmail.com>
2016-10-26 16:11:04 +0000

    0.36
    git-svn-id: svn+ssh://haiku.zarb.org/home/projects/rpm4/svn/trunk@265 971eb68f-4bfb-0310-8326-d2484c010a4c

--- lib/RPM4.pm
+++ lib/RPM4.pm
@@ -30,7 +30,7 @@ use RPM4::Header::Dependencies;
 use RPM4::Header::Files;
 use RPM4::Spec;
 
-our $VERSION = '0.35';
+our $VERSION = '0.36';
 our @ISA = qw(DynaLoader Exporter);
 our @EXPORT = qw(moduleinfo
                  readconfig querytag tagName tagValue expand rpmvercmp
